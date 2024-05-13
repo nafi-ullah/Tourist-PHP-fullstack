525500a893c98be4266a555705186afef880b8ea
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-13 21:49:03 +0600

    auth page ready

--- frontend/src/screens/LoginPage.jsx
+++ frontend/src/screens/LoginPage.jsx
@@ -8,6 +8,9 @@ import { postCred } from "../utils/auth";
 const LoginPage = () => {
     const [regno, setRegno] = useState('');
     const [password, setPassword] = useState('');
+    const [username, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [fullname, setFullName] = useState('');
     const [showSignUp, setShowSignUp] = useState(false);
    
     const navigate = useNavigate();
@@ -22,7 +25,7 @@ const LoginPage = () => {
             
      
           const response = await postCred({
-            regno,
+            username,
             password
           }
          );
@@ -32,24 +35,15 @@ const LoginPage = () => {
         console.log(response);
 
 
-        if(response.token && response.regno != "3040506070" ){
+        if(response.token ){
           if(response.isVoted){
             navigate("/vote-complete", {
                 state: {
                   regno: regno,
                 },
               });
-          }else{
-            navigate("/home", {
-                state: {
-                  regno: regno,
-                },
-              });
           }
-        }else if(response.token && response.regno === "3040506070"){
-            navigate("/killeradmin");
         }
-        
         else{
             // const notify = () => toast("Bad Credential, Try Right Credential");
             // notify();
@@ -70,7 +64,10 @@ const LoginPage = () => {
       const handleSignUp = () => {
        setShowSignUp(true);
       };
-
+      
+      const handleLogin = () => {
+        setShowSignUp(false);
+       };
 
   return (
     <>
@@ -84,8 +81,8 @@ const LoginPage = () => {
     {!showSignUp && <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Log In</h2>
       <div className="relative mb-4">
-        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Registration No</label>
-        <input type="number" id="regno" name="regno" value={regno} onChange={(e) => setRegno(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Username</label>
+        <input type="text" id="username" name="username" value={username} onChange={(e) => setUserName(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <div className="relative mb-4">
         <label htmlFor="Registration-no" class="leading-7 text-sm text-gray-600">Password</label>
@@ -99,16 +96,25 @@ const LoginPage = () => {
     {showSignUp && <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
       <div className="relative mb-4">
-        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Registration No</label>
-        <input type="number" id="regno" name="regno" value={regno} onChange={(e) => setRegno(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Full Name</label>
+        <input type="text" id="fullname" name="fullname" value={fullname} onChange={(e) => setFullName(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
+      <div className="relative mb-4">
+        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <div className="relative mb-4">
+        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Username</label>
+        <input type="text" id="username" name="username" value={username} onChange={(e) => setUserName(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+ 
       <div className="relative mb-4">
         <label htmlFor="Registration-no" class="leading-7 text-sm text-gray-600">Password</label>
         <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <button onClick={handleButtonClick } className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Log in</button>
-      <p className="text-xs text-gray-500 mt-3">Haven't registered yet? <button className="text-button" onClick={handleSignUp}>
-      Sign Up
+      <p className="text-xs text-gray-500 mt-3">Already registered? <button className="text-button" onClick={handleLogin}>
+      Log in
     </button> </p>
     </div>}
     
