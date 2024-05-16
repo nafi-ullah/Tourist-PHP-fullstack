df552f43b8e24b48ceadce8e5be8cfd54e4d2658
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-16 12:43:59 +0600

    components modified

--- frontend/src/screens/LoginPage.jsx
+++ frontend/src/screens/LoginPage.jsx
@@ -4,8 +4,10 @@ import { useNavigate } from "react-router-dom";
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 import { postCred } from "../utils/auth";
-import ListUser from "../components/homecomponents/CreateUser";
+
 import axios from "axios";
+import ListUser from "../components/authComp/CreateUser";
+import LogInForm from "../components/authComp/LogInComp";
 
 
 const LoginPage = () => {
@@ -16,25 +18,7 @@ const LoginPage = () => {
     // const [fullname, setFullName] = useState('');
     const [showSignUp, setShowSignUp] = useState(false);
    
-    const navigate = useNavigate();
-
-    const [inputs, setInputs] = useState([]);
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}));
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        axios.post('http://localhost/api/user/save', inputs).then(function(response){
-            console.log(response.data);
-           
-        });
-        
-    }
-   
+  
 
 
     // const handleButtonClick = async () => {
@@ -111,38 +95,22 @@ const LoginPage = () => {
 
     {!showSignUp && <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Log In</h2>
-      <ListUser />
-      
-      <p className="text-xs text-gray-500 mt-3">Haven't registered yet? <button className="text-button bg-black" onClick={handleSignUp}>
+      <LogInForm/>
+     
+      <p className="text-xs text-gray-500 mt-3">Haven't registered yet? <button className="text-black bg-white" onClick={handleSignUp}>
       Sign Up
     </button> </p>
     </div>}
 
-    {/* {showSignUp && <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+    {showSignUp && <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
-      <div className="relative mb-4">
-        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Full Name</label>
-        <input type="text" id="fullname" name="fullname" value={fullname} onChange={(e) => setFullName(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="Registration-No" class="leading-7 text-sm text-gray-600">Username</label>
-        <input type="text" id="username" name="username" value={username} onChange={(e) => setUserName(e.target.value)}  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
- 
-      <div className="relative mb-4">
-        <label htmlFor="Registration-no" class="leading-7 text-sm text-gray-600">Password</label>
-        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <button onClick={handleButtonClick } className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Log in</button>
-      <p className="text-xs text-gray-500 mt-3">Already registered? <button className="text-button" onClick={handleLogin}>
+      <ListUser/>
+     
+      <p className="text-xs text-gray-500 mt-3">Already registered? <button className="text-black" onClick={handleLogin}>
       Log in
     </button> </p>
    
-    </div>} */}
+    </div>}
     
   </div>
 
